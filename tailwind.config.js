cef389c8714b9a1862cedf37a9151abaaff8d53b
MILAD <miladrealkord@gmail.com>
2024-10-13 14:08:45 +0330

    eit button & font & responsive mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./Harvest/Bahooosh/resources/**/*.blade.php"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./Harvest/Bahooosh/resources/**/*.blade.php"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    screens: {
+        'mobile': '500px',
+        // => @media (min-width: 500px) { ... }
 
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+    },
+}
